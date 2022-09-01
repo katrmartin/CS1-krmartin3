4fac25438255f295173389c3367975aaed1c8333
katrmartin <katrmartin14@gmail.com>
2022-09-01 12:59:35 -0600

    added 2 new files

---
+++ project0/helloworld.cpp
@@ -0,0 +1,10 @@
+// include required libraries/header files
+#include <iostream>
+
+// one main function is always required in a C++ program
+int main() // main entry to the program 
+{
+    // output Hello World!
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
