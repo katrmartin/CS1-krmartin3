1f48751b80a5b18f1eafd51c9467f9eae4d3bc12
katrmartin <katrmartin14@gmail.com>
2022-09-02 21:00:39 -0600

    Final Submission

--- labs/ascii/main.cpp
+++ labs/ascii/main.cpp
@@ -29,17 +29,17 @@ int main()
     //must output: Nice meeting you, <name>!
     cout << "Nice meeting you, " << name << '!' << endl;
 
-cout << "Hope you like my Tom & Jerry's ASCII art...\n\n";
+cout << "Hope you like my Tom & Jerry's ASCII art... :) \n\n";
 
-string line1 = "  |\\_/|   ****************************        (\\_/)\n";
+string line1 = "  |\\_/|    ****************************      (\\_/)\n";
 cout << line1;
-string line2 = " / @ @ \\  *       ASCII Art          *       (='.'=)\n";
+string line2 = " / @ @ \\   *       ASCII Art          *     (='.'=)\n";
 cout << line2;
-string line3 = "( > 0 < ) *  Author: <Katie Martin>  *     ( \" )_( \" )\n";
+string line3 = "( > 0 < )  *  Author: <Katie Martin>  *   ( \" )_( \" )\n";
 cout << line3;
-string line4 = "  >>x<<   *   CS Foundation Course   *\n";
+string line4 = "  >>x<<    *   CS Foundation Course   *\n";
 cout << line4;
-string line5 = "  / O \\   ****************************\n";
+string line5 = "  / O \\    ****************************\n";
 cout << line5;
 
 //FIXME6: use variable to print the second line of the graphic #FIXED
