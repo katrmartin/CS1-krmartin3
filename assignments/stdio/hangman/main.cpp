9d05572137dd793fc592c961b4051853fb1b3f75
katrmartin <katrmartin14@gmail.com>
2022-09-08 13:20:01 -0600

    modified hangman game

--- assignments/stdio/hangman/main.cpp
+++ assignments/stdio/hangman/main.cpp
@@ -6,7 +6,7 @@ stdIO Homework 1
     Updated By: Katie Martin #FIXME1
     Date: 9/7/22 #FIXME2
 
-    This program prints the 7 stages of the hangman game on the console
+    This program prints the 7 stages of the hangman game on the console #FIXME3 #FIXED
 
     Algorithm steps:
     1. Use variables to store data/values
@@ -28,6 +28,9 @@ int main() {
     cout << " Nice meeting you, " << name << '!' << endl;
 
 cout << "This hangman game is under construction... but for now I can show you what the stages will look like! \n ";
+
+cout << "\n Press enter to see Stage 0!";
+cin.get();
 
 cout << "\n Stage 0" << endl;
 
