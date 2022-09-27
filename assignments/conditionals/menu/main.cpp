24e5e956758eec8b9e0dfc02f6239be6cd88fddb
katrmartin <katrmartin14@gmail.com>
2022-09-27 12:59:44 -0600

    worked on menu assignment

--- assignments/conditionals/menu/main.cpp
+++ assignments/conditionals/menu/main.cpp
@@ -15,6 +15,7 @@ Algorithm:
 
 #include <iostream>
 #include <cassert>
+#include <cmath>
 using namespace std;
 
 int num1, num2, num3, num4, num5;
@@ -36,12 +37,51 @@ int largestNum(int num1, int num2, int num3, int num4, int num5) {
         return num5; }
 }
 
+int smallestNum(int num1, int num2, int num3, int num4, int num5) {
+    if (num1 < num2 && num1 < num3 && num1 < num4 && num1 < num5) {
+        return num1; }
+    else if (num2 < num1 && num2 < num3 && num2 < num4 && num2 < num5) {
+        return num2; }
+    else if(num3 < num1 && num3 < num2 && num3 < num4 && num3 < num5) {
+        return num3; }
+    else if(num4 < num1 && num4 < num2 && num4 < num3 && num4 < num5) {
+        return num4; }
+    else (num5 < num1 && num5 < num2 && num5 < num3 && num5 < num4); {
+        return num5; }
+
+}
+
+int floorNum(int num1, int num2, int num3, int num4, int num5) {
+    int sum, floorSum;
+    sum = num1 + num2 + num3 + num4 + num5;
+    floorSum = floor(sum);
+    int even, odd, zero;
+
+    if(floorSum %2 == 0) { //even
+        //cout << floorSum << " The sum of the numbers' floor, " << floorSum << " is even!\n";
+        floorSum = even;
+        return even; }
+    else if(floorSum = 0) {
+        //cout << floorSum << " The sum of the numbers' floor, " << floorSum << " is 0!\n";
+        floorSum = zero;
+        return zero; }
+    else {
+        //cout << floorSum << " The sum of the numbers' floor, " << floorSum << " is odd!\n";
+        floorSum = odd;
+        return odd;
+    }
+    
+}
+
+//need to make it return odd, even, or zero and actually use it
 
 int main() 
 {
     cout << "Please enter 5 whole numbers separated by spaces: ";
     cin >> num1 >> num2 >> num3 >> num4 >> num5;
     cout << largestNum(num1, num2, num3, num4, num5) << " is the larger number!" << endl;
+    cout << smallestNum(num1, num2, num3, num4, num5) << " is the smaller number!" << endl;
+    cout << floorNum(num1, num2, num3, num4, num5) << endl;
 
 
 
