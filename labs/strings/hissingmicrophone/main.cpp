abfd06a766321ff656fdba3405c45ecc3d1bd33d
katrmartin <katrmartin14@gmail.com>
2022-10-20 13:45:25 -0600

    Final Submission

--- labs/strings/hissingmicrophone/main.cpp
+++ labs/strings/hissingmicrophone/main.cpp
@@ -1,5 +1,14 @@
-/*made by: Katie Martin
-updated: 10/19/22*/
+/*
+Kattis - Hissing Microphone Problem
+Updated By: Katie Martin
+Date: 10/20/22
+Problem Statement: https://open.kattis.com/problems/hissingmicrophone
+Algorithm steps:
+1. Read a string
+2. Search for "ss" substring in the string
+    i. if found, print "hiss"
+    ii. otherwise, print "no hiss"
+*/
 
 #include <iostream>
 #include <string>
@@ -7,17 +16,47 @@ updated: 10/19/22*/
 
 using namespace std;
 
-string hiss(string word) {
-    string found, search_str = "ss";
+string hiss (const string &word);
+void test();
+void solve();
+
+string hiss(const string &word) {
+    // FIXME3 - implement algorithm in step 2 #FIXED
+    string search_str;
+    size_t found;
+    search_str = "ss";
+
     found = word.rfind(search_str);
 
-    if (found == string::npos) return search_str;
-        else return search_fail;
+    if (found == string::npos) return "no hiss";
+        else return "hiss";
 }
 
-int main() {
-    string word;
+void solve() {
+string word;
+    // FIXME5 - read string into word #FIXED
     cin >> word;
     cout << hiss(word) << endl;
-    return 0;
+    
+}
+
+int main(int argc, char* argv[]) {
+  if (argc == 2 and string(argv[1]) == "test") {
+    test();
+  }
+    else {
+        solve();
+    }
+        return 0;
+}
+
+void test() {
+    // FIXME4 - write at least two test cases to test hiss() #FIXED
+    assert(hiss("hello") == "no hiss");
+    assert(hiss("list") == "no hiss");
+    assert(hiss("chess") == "hiss");
+    assert(hiss("chriss") == "hiss");
+    assert(hiss("pillow") == "no hiss");
+
+    cerr << "All test cases passed!";
 }
